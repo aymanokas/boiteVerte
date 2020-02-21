2735907d21fec2410686a564c7d85a6e64d50694
aymane <kasbaoui@alphorm.com>
2020-02-21 22:43:58 +0100

    Fix 4 distances

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import car from './car.png'
 import localisationIcon from './localisation.png'
 import l1 from './distancePics/left/l1.png'
@@ -19,56 +19,122 @@ import r3 from './distancePics/right/r3.png'
 import r4 from './distancePics/right/r4.png'
 import speedIcon from './speedIcon.png'
 import issuesIcon from './issueIcon.jpg'
-import {appStyle} from './appStyle'
+import { appStyle } from './appStyle'
 import injectSheet from 'react-jss'
 import io from 'socket.io-client'
 const raspberryAdress = 'http://192.168.0.115:3000'
 const socket = io(raspberryAdress)
 
-
-const App = (props)  => {
-  let {classes}= props
-  const [i , setI] = useState(1)
+const App = props => {
+  let { classes } = props
+  const [front, setFront] = useState(0)
+  const [back, setBack] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
   const [distance, setDistance] = useState(0)
-  socket.on('connect', () => {console.warn('COOOOOONNNNEEEECTEEED')})
-  socket.on('event', (data) => {
+  socket.on('connect', () => {
+    console.warn('COOOOOONNNNEEEECTEEED')
+  })
+  socket.on('event', data => {
     setDistance(data)
-    data > 0 && data < 10 ? setI(1) : data > 10 && data < 15  ? setI(2) : data > 15 && data < 20 ? setI(3) : setI(0)
+    data > 0 && data < 10
+      ? setBack(1)
+      : data > 10 && data < 15
+      ? setBack(2)
+      : data > 15 && data < 20
+      ? setBack(3)
+      : setBack(0)
   })
   return (
     <div className={classes.App}>
       <h1>{distance}</h1>
-      <div className={classes.infosDiv} >
-          <div className={classes.speed}>
-            <img className={classes.speedIcon} src={speedIcon}/>
-            <p>120 km/h</p>
-          </div>
-          <div className={classes.position}>
-            <img className={classes.positionIcon} src={localisationIcon}/>
-            <p>Position label x</p>
-          </div>
-          <div className={classes.issues}>
-            <img className={classes.issuesIcon} src={issuesIcon}/>
-            <p>No Entries</p>
-          </div>
+      <div className={classes.infosDiv}>
+        <div className={classes.speed}>
+          <img className={classes.speedIcon} src={speedIcon} />
+          <p>120 km/h</p>
+        </div>
+        <div className={classes.position}>
+          <img className={classes.positionIcon} src={localisationIcon} />
+          <p>Position label x</p>
+        </div>
+        <div className={classes.issues}>
+          <img className={classes.issuesIcon} src={issuesIcon} />
+          <p>No Entries</p>
+        </div>
       </div>
-      <div className={classes.carDiv} >
-      <div className={classes.leftDistance}>
-        <img className={props.classes.distanceImg} src={l1} alt='booom'/>
-      </div> 
-    <div className={classes.distanceAndCar}>
-    <div className={classes.frontDistance}>
-      <img className={classes.distanceImg} src={f1}/>
-    </div> 
-    <img className={classes.car} src={car}/>
-    <div className={classes.backDistance}>
-        <img className={classes.distanceImg} src={ i === 1 ? b1 : i === 2 ? b2 : i === 3 ? b3 : b4}/>
-      </div> 
-    </div>
-    <div className={classes.rightDistance}>
-      <img className={classes.distanceImg} src={r1}/>
-    </div>   
-    </div>
+      <div className={classes.carDiv}>
+        <div className={classes.leftDistance}>
+          <img
+            className={props.classes.distanceImg}
+            src={
+              left === 1
+                ? l1
+                : left === 2
+                ? l2
+                : left === 3
+                ? l3
+                : left === 4
+                ? l4
+                : null
+            }
+            alt='left distance'
+          />
+        </div>
+        <div className={classes.distanceAndCar}>
+          <div className={classes.frontDistance}>
+            <img
+              className={classes.distanceImg}
+              src={
+                front === 1
+                  ? f1
+                  : front === 2
+                  ? f2
+                  : front === 3
+                  ? f3
+                  : front === 4
+                  ? f4
+                  : null
+              }
+              alt='front distance'
+            />
+          </div>
+          <img className={classes.car} src={car} />
+          <div className={classes.backDistance}>
+            <img
+              className={classes.distanceImg}
+              src={
+                back === 1
+                  ? b1
+                  : back === 2
+                  ? b2
+                  : back === 3
+                  ? b3
+                  : back === 4
+                  ? b4
+                  : null
+              }
+              alt='back distance'
+            />
+          </div>
+        </div>
+        <div className={classes.rightDistance}>
+          <img
+            className={classes.distanceImg}
+            src={
+              right === 1
+                ? r1
+                : right === 2
+                ? r2
+                : right === 3
+                ? r3
+                : right === 4
+                ? r4
+                : null
+            }
+            alt='right distance'
+          />
+        </div>
+      </div>
     </div>
   )
 }
